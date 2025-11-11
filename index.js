55a00fe3dbc7d56f0a2f6d45a529ea578dcf551f
Atik Hassan <atikhassant64@gmail.com>
2025-11-11 18:42:47 +0600

    added the mongodb connected

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yc6y96u.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get('/', (req, res) => {
     res.send("This is my server site ")
 })
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 app.listen(port, () => {
     console.log(`This server is running in port: ${port}`);
 })
